8281a9a76c906a09403945bb4789ede5fd35783e
m00nb0ygame8 <52793898+m00nb0ygame8@users.noreply.github.com>
2025-04-02 20:25:46 -0400

    Added Blank Plugin

---
+++ src/main/java/me/evasive/ego/Ego.java
@@ -0,0 +1,15 @@
+package me.evasive.ego;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Ego extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
